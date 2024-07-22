e8e4eb02b824c161a3c7529382f8b8c2edfc04af
Waheed Barghouthi <waheed.barghouthi@gmail.com>
2024-07-22 16:00:38 +0200

    Update ParseArguments.py

--- Tensile/TensileCreateLib/ParseArguments.py
+++ Tensile/TensileCreateLib/ParseArguments.py
@@ -290,8 +290,8 @@ def parseArguments(input: Optional[List[str]] = None) -> Dict[str, Any]:
     }
 
     if os.environ["CMAKE_CXX_COMPILER"]:
-        print(f"CmakeCXXCompiler is: {args.CmakeCxxCompiler} im not using it anyway")
-        args.CmakeCxxCompiler = os.environ["CMAKE_CXX_COMPILER"]
+        print(f"CmakeCXXCompiler is: {args.CmakeCxxCompiler} and the {args.CxxCompiler} im not using them anyway")
+        args.CxxCompiler = os.environ["CMAKE_CXX_COMPILER"]
     if args.NoEnumerate:
         arguments["ROCmAgentEnumeratorPath"] = False
     if args.GenerateSourcesAndExit:
