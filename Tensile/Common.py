ceda14bffe767ea82dc456c7d2f0d06d1ea703d7
Waheed Barghouthi <waheed.barghouthi@gmail.com>
2024-07-22 14:43:44 +0200

    Update Common.py

--- Tensile/Common.py
+++ Tensile/Common.py
@@ -305,7 +305,7 @@ globalParameters["SeparateArchitectures"] = False # write Tensile library metada
 
 globalParameters["LazyLibraryLoading"] = False # Load library and code object files when needed instead of at startup
 
-globalParameters["IgnoreAsmCapCache"] = true # Ignore checking for discrepancies between derived and cached asm caps
+globalParameters["IgnoreAsmCapCache"] = True # Ignore checking for discrepancies between derived and cached asm caps
 
 globalParameters["ExperimentalLogicDir"] = "/experimental/"
 
